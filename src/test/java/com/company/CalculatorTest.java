7017e5819d000d87bae2c53b7afb9577cb5dcaab
CodeGangsta44 <dovorman44@gmail.com>
2019-10-24 21:39:04 +0300

    Added tests

---
+++ src/test/java/com/company/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.company;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private static Calculator calculator;
+
+    @BeforeClass
+    public static void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void sum() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a + b, calculator.sum(a, b));
+    }
+
+    @Test
+    public void diff() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a - b, calculator.diff(a, b));
+    }
+
+    @Test
+    public void sum() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a * b, calculator.multi(a, b));
+    }
+
+    @Test
+    public void sum() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a / b, calculator.div(a, b));
+    }
+}
