92ab82cb2e7a9eaec8825d9b73e7e326cbee2b56
CodeGangsta44 <dovorman44@gmail.com>
2019-10-31 21:33:13 +0200

    Added test cases for variant #1

---
+++ src/test/java/com/company/ServiceTest.java
@@ -0,0 +1,47 @@
+package com.company;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ServiceTest {
+    private static Service service;
+
+    @BeforeClass
+    public static void init() {
+        service = new ServiceImpl();
+    }
+
+    @Test
+    public void firstMethod() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a + b, service.firstMethod(a, b));
+    }
+
+    @Test
+    public void secondMethod() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a - b, service.secondMethod(a, b));
+    }
+
+    @Test
+    public void thirdMethod() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a * b, service.thirdMethod(a, b));
+    }
+
+    @Test
+    public void fourthMethod() {
+        int a = 5;
+        int b = 6;
+
+        Assert.assertEquals(a / b, service.fourthMethod(a, b));
+    }
+}
