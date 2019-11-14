3b99fc34c9300e3e85d3b4e551f70803c0d78ed0
CodeGangsta44 <dovorman44@gmail.com>
2019-11-14 23:17:19 +0200

    Add logging

--- src/test/java/com/company/ServiceTest.java
+++ src/test/java/com/company/ServiceTest.java
@@ -5,8 +5,12 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServiceTest {
     private static Service service;
+    private static final Logger logger = LoggerFactory.getLogger(ServiceTest.class);
 
     @BeforeClass
     public static void init() {
@@ -18,6 +22,7 @@ public class ServiceTest {
         int a = 5;
         int b = 6;
 
+        logger.info("Tests run: firstMethod - 1 - " + (a + b == service.firstMethod(a, b)));
         Assert.assertEquals(a + b, service.firstMethod(a, b));
     }
 
@@ -26,6 +31,7 @@ public class ServiceTest {
         int a = 5;
         int b = 6;
 
+        logger.info("Tests run: secondMethod - 1 - " + (a - b == service.secondMethod(a, b)));
         Assert.assertEquals(a - b, service.secondMethod(a, b));
     }
 
@@ -34,6 +40,7 @@ public class ServiceTest {
         int a = 5;
         int b = 6;
 
+        logger.info("Tests run: thirdMethod - 1 - " + (a * b == service.thirdMethod(a, b)));
         Assert.assertEquals(a * b, service.thirdMethod(a, b));
     }
 
@@ -42,6 +49,7 @@ public class ServiceTest {
         int a = 5;
         int b = 6;
 
+        logger.info("Tests run: fourthMethod - 1 - " + (a / b == service.fourthMethod(a, b)));
         Assert.assertEquals(a / b, service.fourthMethod(a, b));
     }
 }
